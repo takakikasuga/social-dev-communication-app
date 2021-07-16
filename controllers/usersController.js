499f09e9ba97605fac58125348d43cc5c101521c
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-16 10:38:53 +0900

    DBへの追加、パスワードのハッシュ化

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,59 @@
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
 //@route     POST api/users
 //@desc      Register User
 //@access    Public
-exports.registerUser = (req, res) => {
+exports.registerUser = async (req, res) => {
   const errors = validationResult(req);
   console.log('errors', errors);
   if (!errors.isEmpty())
     return res.status(400).json({ errors: errors.array() });
   console.log('req.body', req.body);
-  res.send('User routes');
+
+  const { name, email, password } = req.body;
+  try {
+    // ユーザー情報の有無の確認
+    // findOne({サーチワード：バリュー});
+    let user = await User.findOne({ email }).collation({
+      locale: 'en',
+      strength: 2
+    });
+    if (user)
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'User already exists!' }] });
+
+    // ユーザーのアバターを取得する（gravatarモジュール）
+    // s：文字列（String）、r：評価（Rating）、d：標準（ Default）
+    const avatar = gravatar.url(email, {
+      size: '200',
+      rating: 'pg',
+      // mmはデフォルトで画像を差し込んでくれる（ミステリーマン）
+      default: 'mm'
+    });
+
+    // ユーザーモデルのインスタンスを作成
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    // パスワードの暗号化
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    console.log('ハッシュ化したuser.password', user.password);
+
+    // データベースにユーザー情報を保管する
+    await user.save();
+
+    res.send('User Registerd');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 };
