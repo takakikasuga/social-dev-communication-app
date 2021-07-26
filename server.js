402aea3ca954d8746e8cfb784f5027eac901bebc
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-26 10:21:56 +0900

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/posts', postsRouter);
 app.use('/api/profile', profileRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(__dirname + '/public'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
